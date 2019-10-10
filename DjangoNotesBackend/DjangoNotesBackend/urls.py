3bfeeeeb1d5f12f5badc8ae167c44076e84ee630
KarlKeisel <karlkeisel83@gmail.com>
2019-10-10 17:29:40 -0400

    Must full commit before eject

--- DjangoNotesBackend/DjangoNotesBackend/urls.py
+++ DjangoNotesBackend/DjangoNotesBackend/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url('', TemplateView.as_view(template_name="index.html"))
 ]
