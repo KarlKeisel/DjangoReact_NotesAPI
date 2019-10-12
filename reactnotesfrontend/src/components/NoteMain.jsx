0f5e50b357cd87be9ed496fc18236e149776e210
KarlKeisel <karlkeisel83@gmail.com>
2019-10-11 22:43:01 -0400

    Fully functional react note maker

--- reactnotesfrontend/src/components/NoteMain.jsx
+++ reactnotesfrontend/src/components/NoteMain.jsx
@@ -6,23 +6,58 @@ import {notes} from "../actions";
 // import {Link} from "react-router-dom";
 
 class NoteMain extends React.Component {
+    state = {
+        text: "",
+        updateNoteId: null,
+    };
+
+    resetForm = () => {
+        this.setState({text: "", updateNoteId: null})
+    };
+
+    selectForEdit = (id) => {
+        let note = this.props.notes[id];
+        this.setState({text: note.text, updateNoteId: id})
+    };
+
+    submitNote = (e) => {
+        e.preventDefault();
+        this.state.updateNoteId === null ?
+            this.props.addNote(this.state.text) :
+            this.props.updateNote(this.state.updateNoteId, this.state.text);
+        this.resetForm();
+        // TODO Find a way to focus on 'input-line' on submit.
+    };
+
     render() {
         return (
             <div>
                 <h2>Welcome to Karl's Note tracker!</h2>
                 <hr/>
 
+                <h3>Add new note</h3>
+                <form onSubmit={this.submitNote}>
+                    <input
+                        className={'input-line'}
+                        value={this.state.text}
+                        placeholder={'Enter new note...'}
+                        onChange={(e) => this.setState({text: e.target.value})}
+                        required/>
+                    <input type={'submit'} value={'Save Note'}/>
+                    <button onClick={this.resetForm}>Reset</button>
+                </form>
+
                 <h3>Notes</h3>
                 <table>
                     <tbody>
-                    {this.props.notes.map(note => (
-                        <tr>
+                    {this.props.notes.map((note, id) => (
+                        <tr key={`note_${id}`}>
                             <td>{note.text}</td>
                             <td>
-                                <button>Edit</button>
+                                <button onClick={() => this.selectForEdit(id)}>Edit</button>
                             </td>
                             <td>
-                                <button>Delete</button>
+                                <button onClick={() => this.props.deleteNote(id)}>Delete</button>
                             </td>
                         </tr>
                     ))}
