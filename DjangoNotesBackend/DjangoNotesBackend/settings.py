0b4ef605067e2bb90badaff7c2eec68e79996c2e
KarlKeisel <karlkeisel83@gmail.com>
2019-10-11 21:39:33 -0400

    Remove react side

--- DjangoNotesBackend/DjangoNotesBackend/settings.py
+++ DjangoNotesBackend/DjangoNotesBackend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'webpack_loader',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'DjangoNotesBackend.urls'
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.dev.json')
+    }
+}
 
 TEMPLATES = [
     {
