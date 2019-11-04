3498501f32b662fca2cb24a726d58adb6547deb0
KarlKeisel <karlkeisel83@gmail.com>
2019-11-03 19:46:26 -0500

    Clean up code for finalizing

--- DjangoNotesBackend/DjangoNotesBackend/settings.py
+++ DjangoNotesBackend/DjangoNotesBackend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import json
+
+with open("../config.json") as config_file:
+    config = json.load(config_file)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'df@)xf61@k=+$w_1%ppbjplj=v6$sdeu7obeg@h!x2e_b4986a'
+SECRET_KEY = config.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
